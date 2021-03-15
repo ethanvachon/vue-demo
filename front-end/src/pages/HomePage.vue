cd7297fd1f5ee44e4dbc8394ca48a7ad10fc9961
ethanvachon <ethanhvachon@gmail.com>
2021-03-14 21:07:40 -0600

    wiring up home

--- front-end/src/pages/HomePage.vue
+++ front-end/src/pages/HomePage.vue
@@ -1,25 +1,29 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo">
-    <h1 class="my-5 bg-dark text-light p-3 rounded d-flex align-items-center">
-      <span class="mx-2 text-white">Vue 3 Starter</span>
-    </h1>
+  <div class="container-fluid">
+    <div class="row">
+      {{ posts }}
+      <div class="col-4" v-for="post in state.posts" :key="post.id" :post="post">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { reactive, computed } from 'vue'
+import { AppState } from '../AppState'
 export default {
-  name: 'Home'
+  name: 'Home',
+  setup() {
+    const state = reactive({
+      posts: computed(() => AppState.accounts)
+    })
+    return {
+      state
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
-.home{
-  text-align: center;
-  user-select: none;
-  > img{
-    height: 200px;
-    width: 200px;
-  }
-}
+
 </style>
